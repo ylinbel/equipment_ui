26784b6d1fb61197ae3cd61e5ba86e80275bde4f
Belle Lin <2025381928@qq.com>
2022-08-29 22:17:52 +0100

    implement register to post data

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -7,51 +7,84 @@ import {
     IonItem, IonInput, IonLabel, IonButton, useIonPicker,
 } from '@ionic/react';
 import React, {useState} from "react";
+import axios from "axios";
 
-const Register: React.FC = () => {
-    const [email, setEmail] = useState<string>();
-    const [name, setName] = useState<string>();
-    const [password, setPassword] = useState<string>();
-    const [repassword, setRePassword] = useState<string>();
-    const [date, setDate] = useState<string>();
-    return (
-        <IonPage>
-            <IonContent>
-                <IonHeader>
-                    <IonToolbar>
-                        <IonTitle class="ion-text-center">Welcome To Dynamics Lab</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
+class Register extends React.Component<any, any> {
+    constructor(props:any) {
+        super(props);
+        this.state = {
+            email: '',
+            name: '',
+            password: '',
+            reEnter: '',
+            utilDate: '',
+        };
+    }
+    createAccount() {
+        const params = JSON.stringify({
+            "email": this.state.email,
+            "name": this.state.name,
+            "password": this.state.password,
+            "utilDate": this.state.utilDate,
+            "userType": "StandardUser"
+        });
+        console.log(params);
+        axios.post('http://localhost:8080/users', params, {
+                headers: {
+                    "Content-Type": "application/json",
+                }})
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
-                <IonItem>
-                    <IonLabel position="stacked">Name</IonLabel>
-                    <IonInput value={name} placeholder="Enter Name" onIonChange={e => setName(e.detail.value!)}> </IonInput>
-                </IonItem>
 
-                <IonItem>
-                    <IonLabel position="stacked" inputMode="email">Email</IonLabel>
-                    <IonInput value={email} placeholder="Enter Email" onIonChange={e => setEmail(e.detail.value!)}> </IonInput>
-                </IonItem>
+    render() {
 
-                <IonItem>
-                    <IonLabel position="stacked" typeof="password">Password</IonLabel>
-                    <IonInput value={password} placeholder="Enter Password" onIonChange={e => setPassword(e.detail.value!)}> </IonInput>
-                </IonItem>
+        return (
+            <IonPage>
+                <IonContent>
+                    <IonHeader>
+                        <IonToolbar>
+                            <IonTitle class="ion-text-center">Welcome To Dynamics Lab</IonTitle>
+                        </IonToolbar>
+                    </IonHeader>
 
-                <IonItem>
-                    <IonLabel position="stacked">Reenter Password</IonLabel>
-                    <IonInput value={repassword} placeholder="Enter Password" onIonChange={e => setRePassword(e.detail.value!)}> </IonInput>
-                </IonItem>
+                    <IonItem>
+                        <IonLabel position="stacked">Name</IonLabel>
+                        <IonInput value={this.state.name} placeholder="Enter Name"
+                                  onIonChange={e => this.setState({name: e})}> </IonInput>
+                    </IonItem>
 
-                <IonItem>
-                    <IonLabel position="stacked">Leaving Date</IonLabel>
-                    <IonInput value={date} placeholder="DD/MM/YYYY" onIonChange={e => setDate(e.detail.value!)}> </IonInput>
-                </IonItem>
-                <br/>
-                <IonButton strong expand="block">Register</IonButton>
-            </IonContent>
-        </IonPage>
-    );
-};
+                    <IonItem>
+                        <IonLabel position="stacked" inputMode="email">Email</IonLabel>
+                        <IonInput value={this.state.email} placeholder="Enter Email"
+                                  onIonChange={e => this.setState({email: e})}> </IonInput>
+                    </IonItem>
+
+                    <IonItem>
+                        <IonLabel position="stacked" typeof="password">Password</IonLabel>
+                        <IonInput value={this.state.password} placeholder="Enter Password"
+                                  onIonChange={e => this.setState({password: e})}> </IonInput>
+                    </IonItem>
+
+                    <IonItem>
+                        <IonLabel position="stacked">Reenter Password</IonLabel>
+                        <IonInput value={this.state.reEnter} placeholder="Enter Password"
+                                  onIonChange={e => this.setState({reEnter: e})}> </IonInput>
+                    </IonItem>
+
+                    <IonItem>
+                        <IonLabel position="stacked">Leaving Date</IonLabel>
+                        <IonInput value={this.state.utilDate} placeholder="DD/MM/YYYY"
+                                  onIonChange={e => this.setState({utilDate: e})}> </IonInput>
+                    </IonItem>
+                    <br/>
+                    <IonButton strong expand="block" onClick={() => this.createAccount()}>Register</IonButton>
+                </IonContent>
+            </IonPage>
+        );
+    };
+}
 
 export default Register;
